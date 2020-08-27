7e713597fdf929eac0a369dc5907e7ebf47ab571
skillup45days5F3 <70306631+skillup45days5F3@users.noreply.github.com>
2020-08-27 09:30:16 +0530

    Add files via upload

---
+++ Dy_ip.java
@@ -0,0 +1,21 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class Dy_ip 
+{
+
+	public static void main(String[] args)
+    {
+      Scanner obj=new Scanner(System.in);
+      //object creation
+      System.out.println("enter number1");
+      int a=obj.nextInt();
+      System.out.println("enter number2");
+      int b=obj.nextInt();
+      int c=a+b;
+      System.out.println("sum value="+c);
+
+	}
+
+}
---
+++ File1.java
@@ -0,0 +1,11 @@
+package pack1;
+
+public class File1 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Java!");
+	}
+
+}
