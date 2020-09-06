2bb174cf881dd5fef3fcba4270378c4453ff0b1a
skillup45days5F3 <70306631+skillup45days5F3@users.noreply.github.com>
2020-09-06 16:37:24 +0530

    Add files via upload

---
+++ Dowhile_loop1.java
@@ -0,0 +1,16 @@
+package pack2_looping_statements;
+
+public class Dowhile_loop1 {
+
+	public static void main(String[] args) {
+		System.out.println("5 TABLE:");
+		int i=1,n=5;
+		do
+		{
+			System.out.println(n+"*"+i+"="+n*i);
+			i++;
+		}while(i<=10);
+
+	}
+
+}
